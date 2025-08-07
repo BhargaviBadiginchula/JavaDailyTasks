9dbecd64a25042681871b1b37a7e209bdb56643c
BhargaviBadiginchula <bhargavibadiginchala2@gmail.com>
2025-08-08 01:39:57 +0530

    task

--- task86/Sorting1DStringArrayInDescending.java
+++ task86/Sorting1DStringArrayInDescending.java
@@ -1,19 +1,22 @@
 import java.util.*;
-public class Sorting1DStringArrayInDescending {
-    public static void main(String[] args){
-        String []str=new String[5];
-        Scanner sc=new Scanner(System.in);
 
-        for(int i=0;i<5;i++){
-          str[i]=sc.next().toLowerCase();
-        }
-        String b[]={"Bhavesh","Shiv","Rudra","Mahakal","Bholenath","Shankar"};
+public class Sorting1DStringArrayInDescending {
+    public static void main(String[] args) {
+        String[] b = {"Bhavesh", "Shiv", "Rudra", "Mahakal", "Bholenath", "Shankar"};
+
+        // Sort array in ascending order first
         Arrays.sort(b);
-        Arrays.sort(str);
-        System.out.println(Arrays.toString(str));
-        System.out.println(Arrays.toString(b));
-        sc.close();
-        
-        
+
+        // Create a new array to store descending order
+        String[] revSortB = new String[b.length];
+
+        // Copy elements from end of sorted array to the new array
+        int index = 0;
+        for (int i = b.length - 1; i >= 0; i--) {
+            revSortB[index++] = b[i];
+        }
+
+        // Print descending sorted array
+        System.out.println(Arrays.toString(revSortB));
     }
 }
